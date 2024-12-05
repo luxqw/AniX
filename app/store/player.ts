462778c6f9c52d79f0c598bd701f3d9382f40857
Kentai Radiquum <kentai.waah@gmail.com>
2024-12-05 18:49:35 +0500

    refactor: player store
    https://github.com/Radiquum/AniX/pull/3#discussion_r1871410991

--- app/store/player.ts
+++ app/store/player.ts
@@ -3,40 +3,37 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 interface userPlayerPreferencesState {
-    voiceover: Record<number, string>;
-    player: Record<number, string>;
-    getPreferredVoiceover: (id: number) => (string | undefined);
-    setPreferredVoiceover: (id: number, voiceover: string) => void;
-    getPreferredPlayer: (id: number) => (string | undefined);
-    setPreferredPlayer: (id: number, player: string) => void;
+  voiceover: Record<number, string>;
+  player: Record<number, string>;
+  getPreferredVoiceover: (id: number) => string | undefined;
+  setPreferredVoiceover: (id: number, voiceover: string) => void;
+  getPreferredPlayer: (id: number) => string | undefined;
+  setPreferredPlayer: (id: number, player: string) => void;
 }
 
-export const useUserPlayerPreferencesStore = create<userPlayerPreferencesState>()(
+export const useUserPlayerPreferencesStore =
+  create<userPlayerPreferencesState>()(
     persist(
-        (set, get) => ({
-            voiceover: {},
-            player: {},
-            getPreferredVoiceover: (id: number) => get().voiceover[id],
-            setPreferredVoiceover: (id: number, voiceover: string) => {
-                let current = get().voiceover
-                current[id] = voiceover
-                set({
-                    voiceover: current,
-                    player: get().player,
-                });
-            },
-            getPreferredPlayer: (id: number) => get().player[id],
-            setPreferredPlayer: (id: number, player: string) => {
-                let current = get().player
-                current[id] = player
-                set({
-                    voiceover: get().voiceover,
-                    player: current,
-                })
-            }
-        }),
-        {
-            name: "player-preferences",
-        }
+      (set, get) => ({
+        voiceover: {},
+        player: {},
+        getPreferredVoiceover: (id: number) => get().voiceover[id],
+        setPreferredVoiceover: (id: number, voiceover: string) => {
+          set({
+            voiceover: { ...get().voiceover, [id]: voiceover },
+            player: get().player,
+          });
+        },
+        getPreferredPlayer: (id: number) => get().player[id],
+        setPreferredPlayer: (id: number, player: string) => {
+          set({
+            player: { ...get().player, [id]: player },
+            voiceover: get().voiceover,
+          });
+        },
+      }),
+      {
+        name: "player-preferences",
+      }
     )
-);
+  );
