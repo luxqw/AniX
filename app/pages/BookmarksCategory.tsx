2e037a99fe96d99bf6fd1f5241494343af4fc905
Kentai Radiquum <kentai.waah@gmail.com>
2024-12-05 20:30:03 +0500

    fix: remove search box from user statistic page lists view

--- app/pages/BookmarksCategory.tsx
+++ app/pages/BookmarksCategory.tsx
@@ -94,61 +94,130 @@ export function BookmarksCategoryPage(props: any) {
 
   return (
     <>
-      <form
-        className="flex-1 max-w-full mx-4"
-        onSubmit={(e) => {
-          e.preventDefault();
-          router.push(`/search?q=${searchVal}&where=list&list=${props.slug}`);
-        }}
-      >
-        <label
-          htmlFor="default-search"
-          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+      {!props.profile_id ? (
+        <form
+          className="flex-1 max-w-full mx-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            router.push(`/search?q=${searchVal}&where=list&list=${props.slug}`);
+          }}
         >
-          Search
-        </label>
-        <div className="relative">
-          <div className="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
-            <svg
-              className="w-4 h-4 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-          </div>
-          <input
-            type="search"
-            id="default-search"
-            className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg ps-10 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Поиск аниме..."
-            required
-            value={searchVal}
-            onChange={(e) => setSearchVal(e.target.value)}
-          />
-          <button
-            type="submit"
-            className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
           >
-            Поиск
-          </button>
-        </div>
-      </form>
+            Search
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="default-search"
+              className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg ps-10 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Поиск аниме..."
+              required
+              value={searchVal}
+              onChange={(e) => setSearchVal(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Поиск
+            </button>
+          </div>
+        </form>
+      ) : (
+        ""
+      )}
       <div className="m-4 overflow-auto">
         <Button.Group>
-          <Button className="whitespace-nowrap" disabled={props.slug == "watching"} color="light" onClick={() => router.push(props.profile_id ? `/profile/${props.profile_id}/bookmarks/watching` : "/bookmarks/watching")}>{props.SectionTitleMapping["watching"]}</Button>
-          <Button className="whitespace-nowrap" disabled={props.slug == "planned"} color="light" onClick={() => router.push(props.profile_id ? `/profile/${props.profile_id}/bookmarks/planned` : "/bookmarks/planned")}>{props.SectionTitleMapping["planned"]}</Button>
-          <Button className="whitespace-nowrap" disabled={props.slug == "watched"} color="light" onClick={() => router.push(props.profile_id ? `/profile/${props.profile_id}/bookmarks/watched` : "/bookmarks/watched")}>{props.SectionTitleMapping["watched"]}</Button>
-          <Button className="whitespace-nowrap" disabled={props.slug == "delayed"} color="light" onClick={() => router.push(props.profile_id ? `/profile/${props.profile_id}/bookmarks/delayed` : "/bookmarks/delayed")}>{props.SectionTitleMapping["delayed"]}</Button>
-          <Button className="whitespace-nowrap" disabled={props.slug == "abandoned"} color="light" onClick={() => router.push(props.profile_id ? `/profile/${props.profile_id}/bookmarks/abandoned` : "/bookmarks/abandoned")}>{props.SectionTitleMapping["abandoned"]}</Button>
+          <Button
+            className="whitespace-nowrap"
+            disabled={props.slug == "watching"}
+            color="light"
+            onClick={() =>
+              router.push(
+                props.profile_id
+                  ? `/profile/${props.profile_id}/bookmarks/watching`
+                  : "/bookmarks/watching"
+              )
+            }
+          >
+            {props.SectionTitleMapping["watching"]}
+          </Button>
+          <Button
+            className="whitespace-nowrap"
+            disabled={props.slug == "planned"}
+            color="light"
+            onClick={() =>
+              router.push(
+                props.profile_id
+                  ? `/profile/${props.profile_id}/bookmarks/planned`
+                  : "/bookmarks/planned"
+              )
+            }
+          >
+            {props.SectionTitleMapping["planned"]}
+          </Button>
+          <Button
+            className="whitespace-nowrap"
+            disabled={props.slug == "watched"}
+            color="light"
+            onClick={() =>
+              router.push(
+                props.profile_id
+                  ? `/profile/${props.profile_id}/bookmarks/watched`
+                  : "/bookmarks/watched"
+              )
+            }
+          >
+            {props.SectionTitleMapping["watched"]}
+          </Button>
+          <Button
+            className="whitespace-nowrap"
+            disabled={props.slug == "delayed"}
+            color="light"
+            onClick={() =>
+              router.push(
+                props.profile_id
+                  ? `/profile/${props.profile_id}/bookmarks/delayed`
+                  : "/bookmarks/delayed"
+              )
+            }
+          >
+            {props.SectionTitleMapping["delayed"]}
+          </Button>
+          <Button
+            className="whitespace-nowrap"
+            disabled={props.slug == "abandoned"}
+            color="light"
+            onClick={() =>
+              router.push(
+                props.profile_id
+                  ? `/profile/${props.profile_id}/bookmarks/abandoned`
+                  : "/bookmarks/abandoned"
+              )
+            }
+          >
+            {props.SectionTitleMapping["abandoned"]}
+          </Button>
         </Button.Group>
       </div>
       <div className="flex items-center justify-between px-4 py-2 border-b-2 border-black dark:border-white">
