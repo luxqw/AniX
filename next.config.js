28f9e3d4ab2206232d4d5ce034877b6fc8214719
Kentai Radiquum <kentai.waah@gmail.com>
2025-02-10 04:46:38 +0500

    add cache-control headers to reduce function invocations

--- next.config.js
+++ next.config.js
@@ -7,4 +7,44 @@ module.exports = withPlausibleProxy({
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/bookmarks/(.+)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=2592000, stale-while-revalidate=86400',
+          },
+        ],
+      },
+      {
+        source: '/collection/(.+)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=2592000, stale-while-revalidate=86400',
+          },
+        ],
+      },
+      {
+        source: '/home/(.+)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=2592000, stale-while-revalidate=86400',
+          },
+        ],
+      },
+      {
+        source: '/profile/(.+)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=2592000, stale-while-revalidate=86400',
+          },
+        ],
+      },
+    ];
+  },
 });
