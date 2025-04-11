6ea87a190f42dd65dc90b4cc28341d06c7e254c5
Radiquum <kentai.waah@gmail.com>
2025-04-11 20:12:21 +0500

    fix svg props

--- app/components/ReleasePlayer/ReleasePlayerCustom.tsx
+++ app/components/ReleasePlayer/ReleasePlayerCustom.tsx
@@ -470,14 +470,14 @@ export const ReleasePlayerCustom = (props: {
                 <g
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-width="1.5"
+                  strokeLinecap="round"
+                  strokeWidth="1.5"
                 >
                   <path
-                    stroke-linejoin="round"
+                    strokeLinejoin="round"
                     d="M14 4.5L12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12c0-4.1 2.468-7.625 6-9.168"
                   />
-                  <path stroke-linejoin="round" d="m7.5 10.5l2.5-2v7" />
+                  <path strokeLinejoin="round" d="m7.5 10.5l2.5-2v7" />
                   <path d="M12.5 13.75v-3.5a1.75 1.75 0 1 1 3.5 0v3.5a1.75 1.75 0 1 1-3.5 0Z" />
                 </g>
               </svg>
@@ -498,14 +498,14 @@ export const ReleasePlayerCustom = (props: {
                 <g
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-width="1.5"
+                  strokeLinecap="round"
+                  strokeWidth="1.5"
                 >
                   <path
-                    stroke-linejoin="round"
+                    strokeLinejoin="round"
                     d="M10 4.5L12 2C6.477 2 2 6.477 2 12s4.477 10 10 10s10-4.477 10-10c0-4.1-2.468-7.625-6-9.168"
                   />
-                  <path stroke-linejoin="round" d="m7.5 10.5l2.5-2v7" />
+                  <path strokeLinejoin="round" d="m7.5 10.5l2.5-2v7" />
                   <path d="M12.5 13.75v-3.5a1.75 1.75 0 1 1 3.5 0v3.5a1.75 1.75 0 1 1-3.5 0Z" />
                 </g>
               </svg>
@@ -544,8 +544,8 @@ export const ReleasePlayerCustom = (props: {
                 <g
                   fill="#fff"
                   stroke="#fff"
-                  stroke-linecap="round"
-                  stroke-width="1.5"
+                  strokeLinecap="round"
+                  strokeWidth="1.5"
                 >
                   <path d="M12 4v16" />
                   <path d="M16 7v10M8 7v10" opacity="0.5" />
